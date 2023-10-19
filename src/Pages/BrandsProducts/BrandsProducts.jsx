514752c437cc434b6036b2f3ff8f259a00593699
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-10-19 14:14:44 +0600

    built a basic logic of displaying slider advertisements images based on clicking brands through database

--- src/Pages/BrandsProducts/BrandsProducts.jsx
+++ src/Pages/BrandsProducts/BrandsProducts.jsx
@@ -4,21 +4,39 @@ import { useLoaderData, useParams } from "react-router-dom";
 const BrandsProducts = () => {
 
     const [products, setProducts] = useState([]);
+    const [advertisements, setAdvertisements] = useState([]);
 
     const brandsProducts = useLoaderData();
-    console.log(brandsProducts);
+    //console.log(brandsProducts);
 
     const brandName = useParams();
-    console.log(brandName);
+    //console.log(brandName);
 
     useEffect(() => {
-        const filtered = brandsProducts.filter(brandProduct => brandProduct.brandName === brandName.brandName)
-        setProducts(filtered);
+        const filteredbrandsProducts = brandsProducts.filter(brandProduct => brandProduct.brandName === brandName.brandName)
+        setProducts(filteredbrandsProducts);
     }, [brandName, brandsProducts])
 
 
+    useEffect(() => {
+        fetch('http://localhost:5000/advertisements')
+            .then(res => res.json())
+            .then(data => {
+                //console.log(data);
+                const filteredAdvertisements = data.filter(advertisement => advertisement.brandName === brandName.brandName)
+                //console.log(filteredAdvertisements);
+                setAdvertisements(filteredAdvertisements)
+            })
+    }, [brandName.brandName])
 
-    console.log(products);
+
+
+    //console.log(products);
+    console.log(advertisements);
+
+    // const filteredAdvertisements = advertisements.filter(advertisement => advertisement.brandName === brandName.brandName)
+
+    //console.log();
 
     // const sp = products.map(product => product.name)
     // console.log(sp);
@@ -26,7 +44,23 @@ const BrandsProducts = () => {
     return (
         <div>
             {
-                products.map(product => <li key={product._id}>{product.name}</li>)
+                products.length === 0 ?
+                <p className="font-3xl font-bold">Sorry! Not Available at That Moment!</p>
+
+                :
+                    <div>
+                        {
+                            advertisements.map(advertisement =>
+                                <li key={advertisement._id}>
+                                    <img className="w-1/2" src={advertisement.advertisementsImage1}></img>
+                                    <img className="w-1/2" src={advertisement.advertisementsImage2}></img>
+                                    <img className="w-1/2" src={advertisement.advertisementsImage3}></img>
+                                </li>)
+                        }
+                        {
+                            products.map(product => <li key={product._id}>{product.name}</li>)
+                        }
+                    </div>
             }
         </div>
     );
