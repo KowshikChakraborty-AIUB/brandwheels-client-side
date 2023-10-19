d4f8d0453f6ed62c592bac3a2cda21a50147f5c7
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2023-10-20 00:14:15 +0600

    built logic of removing products from the my cart page and also displayed the remaining carts list

--- src/Pages/MyCart/MyCart.jsx
+++ src/Pages/MyCart/MyCart.jsx
@@ -1,15 +1,35 @@
+import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 
 const MyCart = () => {
 
     const myCart = useLoaderData();
 
+    const [carts, setCarts] = useState(myCart);
+
     console.log(myCart);
+
+    const handleRemoveFromCart = (_id) => {
+        console.log(_id);
+        fetch(`http://localhost:5000/myCarts/${_id}`, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.deletedCount > 0){
+                toast.success('Removed from cart successfully!')
+            }
+
+            const remainingCarts = carts.filter(cartRemains => cartRemains._id !== _id);
+            setCarts(remainingCarts);
+        })
+    }
 
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12">
             {
-                myCart.map(cart =>
+                carts.map(cart =>
 
                     <div key={cart._id} className="card bg-base-100 shadow-xl">
                         <figure><img src={cart.imageURL} alt="Shoes" /></figure>
@@ -21,11 +41,15 @@ const MyCart = () => {
                             <p className="font-bold">Type: {cart.type}</p>
                             <p className="font-bold">Price: {cart.price}</p>
                             <p className="font-bold">Rating: {cart.rating}</p>
+                            <div className="flex justify-center mt-4">
+                                <button onClick={() => handleRemoveFromCart(cart._id)} className="btn bg-[#FF5C1D] hover:bg-[#FF5C1D]">Remove</button>
+                            </div>
                         </div>
                     </div>
 
                 )
             }
+            <ToastContainer></ToastContainer>
         </div>
     );
 };
